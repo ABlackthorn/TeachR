13bec82e5c1100dc894a1e2abd5cb12e3dce9996
ABlackthorn <amine_baidada@hotmail.com>
2019-03-13 20:22:49 -0400

    UI modification

---
+++ app/src/main/java/com/teachr/teachr/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.teachr.teachr
+
+import android.app.Activity
+import android.os.Bundle
+
+class SignupActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
